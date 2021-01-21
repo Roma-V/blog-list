4cb31709ed28c6f5482aea4c54693b3c59cbbd3f
RomanVasilyev <roman.m.vasilyev@gmail.com>
2021-01-21 20:00:16 +0300

    return to no logs

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -27,13 +27,13 @@
  */
 
 const info = (...params) => {
-  if (process.env.NODE_ENV !== '1test') {
+  if (process.env.NODE_ENV !== 'test') {
     console.log(...params)
   }
 }
 
 const error = (...params) => {
-  if (process.env.NODE_ENV !== '1test') {
+  if (process.env.NODE_ENV !== 'test') {
     console.error(...params)
   }
 }
