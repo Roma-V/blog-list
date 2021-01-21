1b4b4847aa8c049b174c45581a3990026bdfe11e
RomanVasilyev <roman.m.vasilyev@gmail.com>
2021-01-21 19:52:29 +0300

    added console statements in config

--- server/utils/config.js
+++ server/utils/config.js
@@ -38,6 +38,10 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
+
+console.log('node:', process.env.NODE_ENV)
+console.log('mongo:', process.env.TEST_MONGODB_URI)
+
 module.exports = {
   MONGODB_URI,
   PORT
