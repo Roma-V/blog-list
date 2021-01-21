a96cd098329c8c455f8046a2acaffa9406dcf369
RomanVasilyev <roman.m.vasilyev@gmail.com>
2021-01-21 20:57:50 +0300

    resolve local testing vs one on github

--- server/tests/users_api.test.js
+++ server/tests/users_api.test.js
@@ -40,8 +40,11 @@ const api = supertest(app)
  * Setup a function to run before each test
  */
 beforeEach(async () => {
-  await api.post('/api/testing/reset')
-  // await User.deleteMany({})
+  if (process.env.GITHUB) {
+    await api.post('/api/testing/reset')
+  } else  {
+    await User.deleteMany({})
+  }
 
   const users = testHelper.initialUsers.map(user => new User(user))
   const promises = users.map(user => user.save())
