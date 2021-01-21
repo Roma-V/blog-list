00cd05abddb190a14006ad069e8f6e8fb21a59ec
RomanVasilyev <roman.m.vasilyev@gmail.com>
2021-01-21 18:15:20 +0300

    test the test env with logger enabled

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -27,13 +27,13 @@
  */
 
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
+  if (process.env.NODE_ENV !== '1test') {
     console.log(...params)
   }
 }
 
 const error = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
+  if (process.env.NODE_ENV !== '1test') {
     console.error(...params)
   }
 }
